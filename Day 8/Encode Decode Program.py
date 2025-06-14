41d9fa5fc553371b3e0039855dd2229b8ea87d5a
elc006 <elsa.chang006@gmail.com>
2025-06-13 22:09:43 -0700

    Encode Decode Program

---
+++ Day 8/Encode Decode Program.py
@@ -0,0 +1,37 @@
+import Caesar_Cipher_Logo
+print(Caesar_Cipher_Logo.logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+
+    for letter in original_text:
+        if letter not in original_text:
+            output_text += original_text
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+program_re_run = True
+
+while program_re_run:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    restart = input("Would you like to encode/decode other message: yes or no?\n").lower()
+    
+    if restart == 'yes':
+        program_re_run = True
+    else:
+        program_re_run = False
+        print("Goodbye!")
+
+
